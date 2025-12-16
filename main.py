78ee5b5281c74186b87126309b1cab69aea14fab
himanshu12332123 <105910418+himanshu12332123@users.noreply.github.com>
2025-12-16 11:12:00 +0530

    get post

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,8 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/create")
+@app.route("/create",methods=["Get","POST"])
 def create():
+    if request.method == "Post":
+        print(request.files)
     return render_template("create.html")
 
 @app.route("/gallery")
